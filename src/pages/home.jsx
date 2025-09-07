81e1779895a8fcbb04d1ce1e1d08d13a194ddcb6
HuaJun-25 <izuankwii@gmail.com>
2025-09-07 19:24:12 +0800

    home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -294,6 +294,51 @@ const Home = () => {
         return () => { sk.kill(); animation.kill(); gsap.ticker.remove(lenis.raf); };
     }, []);
 
+    // notice區 ---------------------------------------
+    const nplane1 = useRef(null);
+    const nplane2 = useRef(null);
+    const nplane3 = useRef(null);
+    let requestAnimationFrameId = null;
+    let nxForce = 0;
+    let nyForce = 0;
+    const neasing = 0.08;
+    const nspeed = 0.01;
+    const noticeMouseMove = (e) => {
+        const { movementX, movementY } = e;
+        nxForce += movementX * nspeed;
+        nyForce += movementY * nspeed;
+
+        if (requestAnimationFrameId == null) {
+            requestAnimationFrameId = requestAnimationFrame(animate);
+        }
+    };
+    const lerp = (start, target, amount) =>
+        start * (1 - amount) + target * amount;
+    const animate = () => {
+        nxForce = lerp(nxForce, 0, neasing);
+        nyForce = lerp(nyForce, 0, neasing);
+
+        gsap.set(nplane1.current, { x: `+=${nxForce}`, y: `+=${nyForce}` });
+        gsap.set(nplane2.current, {
+            x: `+=${nxForce * 0.5}`,
+            y: `+=${nyForce * 0.5}`
+        });
+        gsap.set(nplane3.current, {
+            x: `+=${nxForce * 0.25}`,
+            y: `+=${nyForce * 0.25}`
+        });
+
+        if (Math.abs(nxForce) < 0.01) nxForce = 0;
+        if (Math.abs(nyForce) < 0.01) nyForce = 0;
+
+        if (nxForce !== 0 || nyForce !== 0) {
+            requestAnimationFrame(animate);
+        } else {
+            cancelAnimationFrame(requestAnimationFrameId);
+            requestAnimationFrameId = null;
+        }
+    };
+
 
     return (
         <>
@@ -328,7 +373,7 @@ const Home = () => {
             <div className="worksinner" ref={wrapperRef}>
                 <div className="works-images" ref={worksimgRef}>
                     {worksimgs.map((item, i) => (
-                        <img key={i} src={item} className="wimage" alt={`img-${i}`} draggable="false"/>
+                        <img key={i} src={item} className="wimage" alt={`img-${i}`} draggable="false" />
                     ))}
                 </div>
             </div>
@@ -342,8 +387,9 @@ const Home = () => {
                             return (
                                 <div key={item.id}
                                     className={(index === 0) ? `item-selimg ${fadeIn ? 'fadein' : ''}` : 'item-nonsel'} onClick={() => moveToFront(index)}>
-                                    <img className='item-img' src={item.ImgSrc} draggable="false"/>
-                                </div>) })}
+                                    <img className='item-img' src={item.ImgSrc} draggable="false" />
+                                </div>)
+                        })}
                     </div>
                     {itemimgs.length > 0 && (
                         <div className='item-txt'>
@@ -362,32 +408,76 @@ const Home = () => {
                         <div className="process-text">
                             <div className="process-info">
                                 <h2>01. 討論</h2>
-                                <p>- 修改請一次性羅列清楚，紅線ok<br/>- 個人作畫習慣顏色皆會有偏差</p>
+                                <p>- 修改請一次性羅列清楚，紅線ok<br />- 個人作畫習慣顏色皆會有偏差<br />- 可以許願，有不想出現的構圖(側臉NG等)需事先告知</p>
                                 <h2>▶02. 構圖</h2>
-                                <p>- 可修改1次<br/>- 確認大致位置分布(簡易十字臉)</p>
+                                <p>- 可修改1次<br />- 確認大致位置分布(簡易十字臉)</p>
                             </div>
                             <div className="process-info">
                                 <h2>▶03. 草稿</h2>
-                                <p>- 可修改2次<br/>- 確認大致氛圍舖色和人物</p>
+                                <p>- 可修改2次<br />- 確認大致氛圍舖色和人物</p>
                             </div>
                             <div className="process-info">
                                 <h2>▶04. 完稿確認</h2>
-                                <p>- 可修改2次<br/>- 不提供構圖大改<br/>- 有少畫/設定錯/小部分修改ok</p>
+                                <p>- 可修改2次<br />- 不提供構圖大改<br />- 有少畫/設定錯/小部分修改ok</p>
                                 <h2>05. 交付</h2>
-                                <p>- 需要修改皆可提，評估後在負擔範圍內不會要求加價<br/>- 提供PNG檔</p>
+                                <p>- 需要修改皆可提，評估後在負擔範圍內不會要求加價<br />- 提供PNG檔</p>
                             </div>
                         </div>
                     </div>
                     <div className="process-right">
                         <div className="process-right-b1">
                             <div className="process-img">
-                                <div className="process-img-item"><img src={process1} alt="" draggable="false"/></div>
-                                <div className="process-img-item"><img src={process2} alt="" draggable="false"/></div>
-                                <div className="process-img-item"><img src="https://images.unsplash.com/photo-1754079132758-5dfb65298934?q=80&w=2196&auto=format&fit=crop" alt="" draggable="false"/></div>
+                                <div className="process-img-item"><img src={process1} alt="" draggable="false" /></div>
+                                <div className="process-img-item"><img src={process2} alt="" draggable="false" /></div>
+                                <div className="process-img-item"><img src="https://images.unsplash.com/photo-1754079132758-5dfb65298934?q=80&w=2196&auto=format&fit=crop" alt="" draggable="false" /></div>
                             </div>
                         </div>
                     </div>
-                </div> 
+                </div>
+            </div>
+
+            {/* notice */}
+            <div className="noticeinner">
+                <div onMouseMove={noticeMouseMove} className="notice-wrap">
+                    <div ref={nplane1} className="notice-plane">
+                        <img src={item1} alt="image" />
+                        <img src={item2} alt="image" />
+                        <img src={item3} alt="image" />
+                    </div>
+                    <div ref={nplane2} className="notice-plane">
+                        <img src={item4} alt="image" />
+                        <img src={item3} alt="image" />
+                        <img src={item2} alt="image" />
+                    </div>
+                    <div ref={nplane3} className="notice-plane">
+                        <img src={item1} alt="image" />
+                        <img src={item4} alt="image" />
+                    </div>
+                    <div className="notice-title">
+                        <h3><span>N</span>OTICE</h3>
+                        <p className="notice-text">
+                            - 不接：官方禁止委託之作品、古風、真人<br/>
+                            - 如需標註 mili/咪哩 都可以<br/>
+                            - 超過付款期限(3日)視同取消委託<br/>
+                            - 工期約為確認匯款後2個月(依實際告知為準)<br/>
+                            - 有修改需求可能會依複雜度延後交稿日期(會事先告知)<br/>
+                            - 交稿日期有變更會事先告知，甲方不同意將會全額退費<br/>
+                            - 未提前告知延期的逾期行為將會全額退費<br/>
+                            - 預計完稿日未包含完稿後確認修改時間<br/>
+                            - 作品會上水印公開，禁止AI商用<br/>
+                            - 圖可依頭貼或排版需求裁切、少量印製贈送收藏，返圖非常感謝🙆<br/>
+                            {/* - 金額大於3000可先匯訂金1000<br/> */}
+                            </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* contact */}
+            <div className="contactinner">
+                <div className="contact-wrap">
+
+                </div>
+
             </div>
         </>
     );
